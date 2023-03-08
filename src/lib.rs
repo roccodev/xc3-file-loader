66e68c87bd0ac9b4033881f108cef6fb4d50edcc
RoccoDev <hey@rocco.dev>
2023-03-08 02:20:19 +0100

    file: add replacement logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,53 @@
 use std::ffi::CStr;
 
-use skyline::nn::oe;
-use skyline::{
-    hook,
-    hooks::{InlineCtx, Region},
-};
+use crate::loader::FileLoader;
+use skyline::hook;
 
+static mut FILE_LOADER: Option<FileLoader> = None;
+
+/// Alias for println!, but disabled on release profiles
+macro_rules! dbg_println {
+    ($($arg:tt)*) => {{
+        #[cfg(debug_assertions)]
+        println!($($arg)*);
+    }};
+}
+
+mod hash;
 mod loader;
 
 // ml::DevFileArchiver::getFileInfo
 #[hook(offset = 0x01164f58)]
-unsafe fn test_get_file_info(p1: u64, p2: u32, name: *const u8, p4: u64) -> u64 {
+unsafe fn block_file_load(p1: u64, p2: u32, name: *const u8, p4: u64) -> u32 {
     let file_name = CStr::from_ptr(name as *const _);
-    let st = file_name.to_string_lossy();
-    println!("LOADING: {st}");
-    if st.contains("menu.bdat") {
-        return 0;
+    if let Ok(file_name) = file_name.to_str() {
+        if FILE_LOADER.as_ref().unwrap().is_blocked(file_name) {
+            // By hiding the file from all archives, we make the game look for it in the romfs
+            // directories. Priority is given to DLC in descending order, but loading from the base
+            // game is also supported, should no DLC romfs have the file.
+            dbg_println!("[XC3-Files] Blocking {file_name}");
+            return 0;
+        }
     }
     call_original!(p1, p2, name, p4)
 }
 
 #[skyline::main(name = "xc3_file_loader")]
 pub fn main() {
-    println!("[XC3-Files] Loading...");
+    dbg_println!("[XC3-Files] Loading...");
 
-    skyline::install_hooks!(test_get_file_info);
+    unsafe {
+        let loader = match FileLoader::import_all() {
+            Ok(loader) => loader,
+            Err(id) => {
+                println!("FS error while reading files: {id}");
+                return;
+            }
+        };
+        FILE_LOADER = Some(loader);
+    }
 
-    println!("[XC3-Files] Loaded!");
+    skyline::install_hooks!(block_file_load);
+
+    dbg_println!("[XC3-Files] Loaded!");
 }
---
+++ src/loader.rs
@@ -0,0 +1,132 @@
+use skyline::nn::fs;
+use std::borrow::Borrow;
+use std::ffi::{CStr, CString};
+use std::mem::MaybeUninit;
+
+use crate::hash::{crc32, PreHashedSet};
+
+/// Top-level paths to skip when matching romfs paths
+static TOP_LEVEL_BLACKLIST: [&str; 5] = ["bf3.ard", "bf3.arh", "movie", "sound", "skyline"];
+
+#[derive(Default)]
+pub struct FileLoader {
+    block_list: PreHashedSet<u32>,
+}
+
+struct DirHandle {
+    handle: fs::DirectoryHandle,
+}
+
+type Result<T> = std::result::Result<T, u32>;
+
+macro_rules! nn_try {
+    ($func:expr) => {
+        match $func {
+            0 => {}
+            code => return Err(code),
+        }
+    };
+}
+
+impl FileLoader {
+    pub unsafe fn import_all() -> Result<Self> {
+        let mut loader = Self::default();
+        loader.import_dir("rom:/", 0)?;
+        Ok(loader)
+    }
+
+    pub fn is_blocked(&self, file_name: &str) -> bool {
+        self.block_list.contains(&crc32(file_name))
+    }
+
+    unsafe fn import_dir(&mut self, path: &str, level: usize) -> Result<()> {
+        let handle = DirHandle::new(
+            CString::new(if level != 0 {
+                &path[..path.len() - 1]
+            } else {
+                path
+            })
+            .unwrap(),
+        )?;
+        let entry_count = handle.get_entry_count()?;
+        let mut new_count = 0i64;
+        let mut entries: Vec<fs::DirectoryEntry> = Vec::with_capacity(entry_count);
+        nn_try!(fs::ReadDirectory(
+            &mut new_count,
+            entries.as_mut_ptr() as *mut _,
+            handle.inner(),
+            entry_count as i64,
+        ));
+        // fs::ReadDirectory returns the number of entries it's written to the buffer, which is
+        // always <= the value that was passed as the last parameter.
+        entries.set_len(new_count as usize);
+        // Close directory early
+        drop(handle);
+
+        // IMPORTANT: keep the reference/.iter() here.
+        // The DirectoryEntry struct is very heavy (784 bytes), and our environment is really
+        // sensitive wrt stack space, it's very easy to overflow.
+        for entry in entries.iter() {
+            let ty = entry.type_;
+            // The DirectoryEntry struct guarantees that the path is null-terminated
+            let name = CStr::from_ptr(entry.name.as_ptr() as *const _);
+            let name = name.to_string_lossy();
+
+            if level == 0 && TOP_LEVEL_BLACKLIST.contains(&name.borrow()) {
+                continue;
+            }
+
+            if ty == fs::DirectoryEntryType_DirectoryEntryType_Directory as u8 {
+                self.import_dir(&format!("{path}{name}/"), level + 1)?;
+            } else {
+                self.register_file(&format!("{path}{name}"));
+            }
+        }
+        Ok(())
+    }
+
+    fn register_file(&mut self, path: &str) {
+        assert!(path.len() >= 4); // rom:/<file name>
+        let path = &path[4..];
+        let hash = crc32(path);
+        dbg_println!("[XC3-Files] Registering {path}");
+        if !self.block_list.insert(hash) {
+            // The game also uses CRC-32 internally to cache resources. It's likely that
+            // the game would have also run into issues with this collision.
+            dbg_println!("Hash collision for path {path} ({hash:08X})");
+        }
+    }
+}
+
+impl DirHandle {
+    unsafe fn new(path: CString) -> Result<Self> {
+        let mut handle: fs::DirectoryHandle = MaybeUninit::zeroed().assume_init();
+        nn_try!(fs::OpenDirectory(
+            &mut handle as *mut _,
+            path.as_ptr() as *const _,
+            fs::OpenDirectoryMode_OpenDirectoryMode_All as i32
+        ));
+        Ok(Self { handle })
+    }
+
+    fn inner(&self) -> fs::DirectoryHandle {
+        self.handle
+    }
+
+    fn get_entry_count(&self) -> Result<usize> {
+        let mut count = 0i64;
+        unsafe {
+            nn_try!(fs::GetDirectoryEntryCount(
+                &mut count as *mut _,
+                self.handle
+            ));
+        }
+        Ok(count as usize)
+    }
+}
+
+impl Drop for DirHandle {
+    fn drop(&mut self) {
+        unsafe { fs::CloseDirectory(self.handle) }
+    }
+}
